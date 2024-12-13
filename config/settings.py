dbad9f5fcbb246fdac77526fba7cfe7e90732335
Elena Vedeneva <astroshelest@gmail.com>
2024-12-13 19:44:58 +0100

    Add PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
 # custom_packages
 INSTALLED_APPS += [
     'rest_framework',
-    'django-filters',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST', default='localhost'),
+        'PORT': env.str('DB_PORT', default=5432),
     }
 }
 
